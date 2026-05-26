58c52b3afd7a4a152e91f741b38f20da17537383
martapanc <marta.panc@gmail.com>
2026-05-26 16:59:12 +0200

    Improve photo size

--- src/components/organisms/home/Photo.tsx
+++ src/components/organisms/home/Photo.tsx
@@ -9,9 +9,9 @@ import styles from './home.module.css';
 import clsxm from '@/lib/clsxm';
 
 const blackSignature =
-  'https://res.cloudinary.com/dwrurydlt/image/upload/f_auto,q_auto,w_250/v1693066829/signature_b64d54de16.webp';
+  'https://res.cloudinary.com/dwrurydlt/image/upload/f_auto,q_auto,w_350/v1693066829/signature_b64d54de16.webp';
 const whiteSignature =
-  'https://res.cloudinary.com/dwrurydlt/image/upload/f_auto,q_auto,w_250/v1693556940/signature_white_60dbdcd21f.webp';
+  'https://res.cloudinary.com/dwrurydlt/image/upload/f_auto,q_auto,w_350/v1693556940/signature_white_60dbdcd21f.webp';
 
 const Photo = () => {
   const { theme } = useTheme();
@@ -28,11 +28,11 @@ const Photo = () => {
   }, [theme]);
 
   return (
-    <div className='flex h-[300px] w-full justify-center items-center lg:h-[280px] lg:w-1/2 lg:justify-end mt-0 md:mt-8 rounded-md'>
+    <div className='flex h-75 w-full justify-center items-center lg:h-70 lg:w-1/2 lg:justify-end mt-0 md:mt-8 rounded-md'>
       <div className='flex flex-col items-center mb-2'>
         <img
           className={clsxm('mb-4 rounded-full', avatar)}
-          src='https://res.cloudinary.com/dwrurydlt/image/upload/f_auto,q_auto,w_190/v1694699676/avatar_md_nobg_ef5553f75b.png'
+          src='https://res.cloudinary.com/dwrurydlt/image/upload/f_auto,q_auto,w_390/v1694699676/avatar_md_nobg_ef5553f75b.png'
           alt='Avatar'
           width={190}
           style={{ height: '190px' }}
